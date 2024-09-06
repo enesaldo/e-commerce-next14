7a3a32d69c41b5b301c6df466c884c403d053c3f
Enes Polat <enespolat@Eness-MacBook-Air.local>
2024-09-06 15:24:07 +0300

    add search page and change favicon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,43 @@
+"use client";
+
 import Link from "next/link";
 import MobileMenu from "./MobileMenu";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Header() {
+  const [query, setQuery] = useState<string>("");
+  const router = useRouter();
+
+  const handleSearch = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (query.trim()) {
+      router.push(`/search?query=${query}`);
+    }
+  };
   return (
     <header className="bg-primary p-4 flex justify-between items-center">
       <Link className="text-white font-bold text-2xl" href="/">
         E-Ticaret
       </Link>
-      <MobileMenu />
+      <form
+        onSubmit={handleSearch}
+        className="flex sm:flex-row gap-2 flex-col md:m-0 ml-10 max-md:w-full items-center"
+      >
+        <input
+          type="text"
+          placeholder="Search..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          className="p-2 rounded  md:w-96 w-full shadow-lg "
+        />
+        <button
+          type="submit"
+          className="sm:m-0 m-2 sm:p-2 p-0.5 sm:w-max w-full  sm:text-base text-xs shadow-lg text-white rounded"
+        >
+          Search
+        </button>
+      </form>
     </header>
   );
 }
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -4,15 +4,18 @@ import Link from "next/link";
 
 export default function ProductCard({ product }: any) {
   return (
-    <div className="border rounded-lg p-4 flex flex-col">
+    <div
+      className="
+     rounded-lg p-4 flex flex-col shadow-lg"
+    >
       <Image
         height={100}
         width={100}
         src={product.thumbnail}
         alt={product.title}
-        className="h-full object-contain mb-4"
+        className="h-full object-contain w-full  mb-4"
       />
-      <h2 className="text-lg font-semibold">{product.title}</h2>
+      <h2 className="text-lg font-semibold  h-20 truncate">{product.title}</h2>
       <p className="text-gray-500">{product.price} $</p>
       <div className="flex justify-between items-center">
         <Link
