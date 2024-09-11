58888c7d89b7b8b3fd4f0f02206b6c051857c542
Enes Polat <enespolat@Eness-MacBook-Air.local>
2024-09-11 18:37:29 +0300

    css changed and add error search

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -11,12 +11,22 @@ export default function SearchPage() {
   const [products, setProducts] = useState<Product[]>([]);
   const searchParams = useSearchParams();
   const query = searchParams.get("query");
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchSearchResults = async () => {
       if (query) {
-        const results = await searchProducts(query);
-        setProducts(results);
+        try {
+          const results = await searchProducts(query);
+          if (results.length === 0) {
+            setErrorMessage(`No products found for "${query}".`);
+          } else {
+            setProducts(results);
+            setErrorMessage(null);
+          }
+        } catch (error) {
+          setErrorMessage("An error occurred while searching for products.");
+        }
       }
     };
 
@@ -25,16 +35,31 @@ export default function SearchPage() {
 
   return (
     <div>
-      <h1 className="text-2xl font-bold mb-4">Search Results for "{query}"</h1>
-
-      <div
-        className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5
+      {errorMessage ? (
+        <div className="flex justify-center items-center text-center h-64">
+          <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 items-center rounded relative max-w-xl text-center shadow-lg">
+            <strong className="font-bold">Oops! </strong>
+            <span className="block sm:inline">{errorMessage}</span>
+          </div>
+        </div>
+      ) : (
+        <div className="container mx-auto">
+          <div className=" w-full shadow-lg h-16 items-center flex flex-row my-8 text-center ">
+            <div className="bg-primary w-2 h-full "> </div>
+            <h1 className="text-2xl capitalize text-gray-400 font-bold px-10 ">
+              Search Results for "{query}"
+            </h1>
+          </div>
+          <div
+            className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5
        gap-4"
-      >
-        {products.map((product: Product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </div>
+          >
+            {products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
