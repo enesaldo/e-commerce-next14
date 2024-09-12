4205eb4354943c296be0c859598f3953ae9c5bf0
Enes Polat <enespolat@Eness-MacBook-Air.local>
2024-09-12 15:41:31 +0300

    footer desing changed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <div className="bg-primary w-full items-center justify-center flex px-4 flex-col lg:pt-10 pt-2 pb-4">
-      <div className="flex align-center justify-center md:text-xl text-xs text-white gap-4">
+      <div className="flex align-center justify-center md:text-lg text-xs text-white gap-4">
         <Link href="/" className="text-uppercase">
           PRIVACY POLICY
         </Link>
@@ -16,8 +16,8 @@ export default function Footer() {
           ABOUT BuyerFe.
         </Link>
       </div>
-      <span className="text-white md:text-xl text-xs pt-2 ">
-        &copy; 2022 BuyerFe. All Rights Reserved.
+      <span className="text-white md:text-lg text-xs pt-2 ">
+        &copy; 2024 BuyerFe. All Rights Reserved.
       </span>
     </div>
   );
