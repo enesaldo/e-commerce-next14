9be6a91dc37ac39e2abf8609eca31793ffcdf585
Enes Polat <enespolat@Eness-MacBook-Air.local>
2024-09-24 18:03:32 +0300

    change text size footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,15 +4,15 @@ export default function Footer() {
   return (
     <div className="bg-primary w-full items-center justify-center flex px-4 flex-col lg:pt-10 pt-2 pb-4">
       <div className="flex align-center justify-center md:text-lg text-xs text-white gap-4">
-        <Link href="/" className="text-uppercase">
+        <Link href="/" className="text-xs lg:text-lg">
           PRIVACY POLICY
         </Link>
         <div className="text-xs md:text-lg"></div>
-        <Link href="/" className="text-uppercase">
+        <Link href="/" className="text-xs lg:text-lg">
           TERM OF SERVICE
         </Link>
         <div className="text-xs md:text-lg"></div>
-        <Link href="/" className="text-uppercase">
+        <Link href="/" className="text-xs lg:text-lg">
           ABOUT BuyerFe.
         </Link>
       </div>
