8792bd0de1ea1ef96c7c954ed7477dc639865737
Enes Polat <enespolat@Eness-MacBook-Air.local>
2024-09-30 16:19:09 +0300

    pagination responsive

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -12,34 +12,37 @@ export default function Pagination({
   for (let i = 1; i <= totalPages; i++) {
     pageNumbers.push(i);
   }
+
   return (
-    <div className="flex justify-center items-center space-x-2 mt-6">
+    <div className="flex justify-center items-center space-x-2 mt-6 overflow-x-auto">
       <a
         href={`/?page=${currentPage - 1}`}
-        className={`px-4 py-2 bg-orange-500 text-white rounded ${
+        className={`px-3 py-1 lg:text-sm text-xs bg-orange-500 text-white rounded ${
           currentPage === 1 ? "opacity-50 cursor-not-allowed" : ""
         }`}
       >
         Previous
       </a>
 
-      {pageNumbers.map((number) => (
-        <a
-          key={number}
-          href={`/?page=${number}`}
-          className={`px-4 py-2 rounded ${
-            currentPage === number
-              ? "bg-orange-500 text-white"
-              : "bg-gray-200 text-gray-700 hover:bg-orange-500 hover:text-white"
-          }`}
-        >
-          {number}
-        </a>
-      ))}
+      <div className="flex space-x-1 overflow-x-auto">
+        {pageNumbers.map((number) => (
+          <a
+            key={number}
+            href={`/?page=${number}`}
+            className={`px-3 py-1 text-sm rounded ${
+              currentPage === number
+                ? "bg-orange-500 text-white"
+                : "bg-gray-200 text-gray-700 hover:bg-orange-500 hover:text-white"
+            }`}
+          >
+            {number}
+          </a>
+        ))}
+      </div>
 
       <a
         href={`/?page=${currentPage + 1}`}
-        className={`px-4 py-2 bg-orange-500 text-white rounded ${
+        className={`px-3 py-1 lg:text-sm text-xs bg-orange-500 text-white rounded ${
           currentPage === totalPages ? "opacity-50 cursor-not-allowed" : ""
         }`}
       >
